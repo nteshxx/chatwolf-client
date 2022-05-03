cc0bb167259202d7571ff033bc954ffa0e2d336c
Nitesh Yadav <nteshxx@gmail.com>
2022-05-03 17:55:34 +0530

    about page added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../styles/about.css';
+import github from '../assets/github.svg';
+import linkedin from '../assets/linkedin.svg';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <h2>Nitesh Yadav</h2>
+      <p>MERN Stack Developer</p>
+      <div className='links'>
+        <a href='https://github.com/nteshxx/file-sher-client'><img src={github} alt=''/></a>
+        <a href='https://www.linkedin.com/in/nteshxx/'><img src={linkedin} alt=''/></a>
+      </div>
+      <p>Thanks for visiting!</p>
+    </div>
   )
 }
 
-export default About
+export default React.memo(About);
