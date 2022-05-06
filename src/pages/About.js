731ee0576e9ae7a2f97b294a4649f26406da71cb
Nitesh Yadav <nteshxx@gmail.com>
2022-05-06 16:10:33 +0530

    about page css fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,15 +6,21 @@ import linkedin from '../assets/linkedin.svg';
 const About = () => {
   return (
     <div className="about">
-      <h2>Nitesh Yadav</h2>
-      <p>MERN Stack Developer</p>
-      <div className='links'>
-        <a href='https://github.com/nteshxx/file-sher-client'><img src={github} alt=''/></a>
-        <a href='https://www.linkedin.com/in/nteshxx/'><img src={linkedin} alt=''/></a>
+      <div className="about-container">
+        <h2>Nitesh Yadav</h2>
+        <p>MERN Stack Developer</p>
+        <div className="links">
+          <a href="https://github.com/nteshxx/chatwolf-client">
+            <img src={github} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/nteshxx/">
+            <img src={linkedin} alt="" />
+          </a>
+        </div>
+        <p>Thanks for visiting!</p>
       </div>
-      <p>Thanks for visiting!</p>
     </div>
-  )
-}
+  );
+};
 
 export default React.memo(About);
